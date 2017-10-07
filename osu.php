c0679800caccfe1c7b3e74e8305de7affffb51a0
Gregg <greggkrzykowski@gmail.com>
2017-10-07 14:17:01 -0500

    yay! first stuff
    First implementation of the API. Also changed the way get_json works, and removed the baseURL var

--- osu.php
+++ osu.php
@@ -2,19 +2,58 @@
 
 class Osu {
     private $token;
-    private $baseURL = "https://osu.ppy.sh/api/";
+
 
     function __construct($token) {
         $this->token = $token;
     }
 
-    public function get_user($username) {
-        return $this->get_json("get_user?k=" . $token . "&u=" . $username);
+    public function get_beatmap($id, $mode = 0) {
+        global $token;
+
+        return $this->get_json("/get_beatmaps?k=" . $this->token . "&b=" . $id . "&m=" . $mode)[0];
+    }
+
+    public function get_user($username, $mode = 0) {
+        global $token;
+
+        return $this->get_json("/get_user?k=" . $this->token . "&u=" . $username . "&m=" . $mode)[0];
+    }
+
+    public function get_scores($id, $limit = 50, $mode = 0) {
+        global $token;
+
+        return $this->get_json("/get_scores?k=" . $this->token . "&b=" . $id . "&m=" . $mode . "&m=" . $mode);
+    }
+
+    public function get_user_best($id, $limit = 10, $mode = 0) {
+        global $token;
+        
+        return $this->get_json("/get_user_best?k=" . $this->token . "&limit=" . $limit . "&mode=" . $mode);
+    }
+
+    public function get_user_recent($userID, $limit = 10, $mode = 0) {
+        global $token;
+        
+        return $this->get_json("/get_user_recent?k=" . $token . "&limit=" . $limit . "&m=" . $mode);
+    }
+
+
+    public function get_match($matchID) {
+        global $token;
+
+        return $this->get_json("/get_match?k=" . $token . "&mp=" . $matchID)[0];
+    }
+
+    public function get_replay($id, $userID, $mode = 0) {
+        global $token;
+
+        return $this->get_json("/get_replay?k=" . $token , "&u=" . $userID . "&m=" . $mode);
     }
 
     private function get_json($url) {
         ini_set("allow_url_fopen", 1);
-        $json = json_decode(file_get_contents($baseURL . $url), true);
+        $json = json_decode(file_get_contents("https://osu.ppy.sh/api" . $url), true);
         ini_set("allow_url_fopen", 0);
 
         return $json;
